22c77eb8e7e3a080ba3a6bc16c3bc01fd1d1449a
Yumarx Polanco <jumarpolanco@gmail.com>
2016-11-30 10:17:12 -0400

    requiring bot on main js

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
 'use strict';
 
-var TelegramBot = require('node-telegram-bot-api');
-
-
-//telegram bot
-var token = '239421563:AAFmMyYsF4W7ltmgx_4T8b1T4mJaEJJ709o';
-// Setup polling way
-var bot = new TelegramBot(token, {polling: true});
-
-bot.on('message', function (msg) {
-  var chatId = msg.chat.id;
-  
-  bot.sendMessage(chatId, "muy pronto este bot funcionara bien! :D");
-});
+require('./app/telegram-bot.js');
